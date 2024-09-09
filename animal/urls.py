78848fedb91219834c05c7ae416bfe98c58cd764
Jadson Silva <jadsonjosesilva@gmail.com>
2024-09-09 19:52:45 -0300

    Update urlpatterns

---
+++ animal/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('animais/', views.lista_animais, name='lista_animais'),
+    path('animais/<int:pk>/', views.detalhe_animal, name='detalhe_animal'),
+    path('animais/novo/', views.criar_animal, name='criar_animal'),
+    path('animais/<int:pk>/editar/', views.editar_animal, name='editar_animal'),
+    path('animais/<int:pk>/deletar', views.deletar_animal, name='deletar_animal'),
+
+    # Adicione rotas semelhantes para Adotante, Adoção e Abrigo
+]
