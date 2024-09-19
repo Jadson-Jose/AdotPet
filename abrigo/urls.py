8f1380e71726d899d28d2ff2eb638ba98695aa30
Jadson Silva <jadsonjosesilva@gmail.com>
2024-09-19 19:06:02 -0300

    update

--- abrigo/urls.py
+++ abrigo/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('abrigo/', views.lista_abrigos, name='lista_abrigos'),
-    path('abrigo/<int:pk>/', views.detalhe_abrigo, name='detalhe_abrigo'),
-    path('abrigo/novo/', views.criar_abrigo, name='criar_abrigo'),
-    path('abrigo/<int:pk>/editar/', views.editar_abrigo, name='editar_abrigo'),
-    path('abrigo/<int:pk>/deletar_abrigo/',
+    path('abrigos/', views.lista_abrigo, name='lista_abrigo'),
+    path('abrigos/<int:pk>/', views.detalhe_abrigo, name='detalhe_abrigo'),
+    path('abrigos/novo/', views.criar_abrigo, name='criar_abrigo'),
+    path('abrigos/<int:pk>/editar/', views.editar_abrigo, name='editar_abrigo'),
+    path('abrigos/<int:pk>/deletar_abrigo/',
          views.deletar_abrigo, name='deletar_abrigo'),
 ]
