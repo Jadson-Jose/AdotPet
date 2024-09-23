4eaf2d3b094f462300add14a2e264db4ba58e31a
Jadson Silva <jadsonjosesilva@gmail.com>
2024-09-23 16:26:32 -0300

    Sinc

--- abrigo/urls.py
+++ abrigo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('abrigos/', views.lista_abrigo, name='lista_abrigo'),
+    path('abrigos/', views.lista_abrigos, name='lista_abrigos'),
     path('abrigos/<int:pk>/', views.detalhe_abrigo, name='detalhe_abrigo'),
     path('abrigos/novo/', views.criar_abrigo, name='criar_abrigo'),
     path('abrigos/<int:pk>/editar/', views.editar_abrigo, name='editar_abrigo'),
